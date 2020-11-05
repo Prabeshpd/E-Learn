3b2caa9eb26a63f762b9a14e084abdbb57ce2778
Prabeshpd <39412015+Prabeshpd@users.noreply.github.com>
2020-11-05 09:34:40 +0545

    configure webpack for react and add lint

---
+++ client/webpack.config.js
@@ -0,0 +1,97 @@
+const path = require("path");
+const webpack = require("webpack");
+const Dotenv = require("dotenv-webpack");
+
+const MiniCssExtractPlugin = require("mini-css-extract-plugin");
+
+const isDevelopment = (process.env.NODE_ENV || "development") === "development";
+
+module.exports = {
+  entry: ["./src/index.js"],
+  mode: isDevelopment ? "development" : "production",
+  plugins: [
+    new webpack.HotModuleReplacementPlugin(),
+    new MiniCssExtractPlugin({
+      filename: isDevelopment ? "[name].css" : "[name].[hash].css",
+      chunkFilename: isDevelopment ? "[id].css" : "[id].[hash].css",
+    }),
+    new Dotenv(),
+  ],
+  devtool: isDevelopment ? "source-map" : false,
+  module: {
+    rules: [
+      {
+        test: /\.(js|jsx)$/,
+        exclude: /(node_modules|bower_components)/,
+        loader: "babel-loader",
+        options: { presets: ["@babel/env"] },
+      },
+      {
+        test: /\.(png|jpe?g|gif)$/i,
+        use: [
+          {
+            loader: "file-loader",
+          },
+        ],
+      },
+      {
+        test: /\.css$/,
+        use: ["style-loader", "css-loader"],
+      },
+      {
+        test: /\.module\.s(a|c)ss$/,
+        loader: [
+          isDevelopment ? "style-loader" : MiniCssExtractPlugin.loader,
+          {
+            loader: "css-loader",
+            options: {
+              modules: true,
+              sourceMap: isDevelopment,
+              esModule: true,
+              hmr: isDevelopment,
+            },
+          },
+          {
+            loader: "sass-loader",
+            options: {
+              sourceMap: isDevelopment,
+            },
+          },
+        ],
+      },
+      {
+        test: /\.s(a|c)ss$/,
+        exclude: /\.module.(s(a|c)ss)$/,
+        loader: [
+          isDevelopment ? "style-loader" : MiniCssExtractPlugin.loader,
+          "css-loader",
+          {
+            loader: "sass-loader",
+            options: {
+              sourceMap: isDevelopment,
+            },
+          },
+        ],
+      },
+    ],
+  },
+  resolve: {
+    alias: {
+      "@Components": path.resolve(__dirname, "src/components/"),
+    },
+    extensions: ["*", ".js", ".jsx", ".scss", ".css"],
+  },
+  output: {
+    path: path.resolve(__dirname, "./public/dist"),
+    publicPath: "/dist/",
+    filename: "bundle.js",
+  },
+  devServer: {
+    historyApiFallback: true,
+    contentBase: path.join(__dirname, "public/"),
+    port: 3001,
+    publicPath: "http://localhost:3001/dist/",
+    hotOnly: true,
+    host: "0.0.0.0",
+  },
+};
